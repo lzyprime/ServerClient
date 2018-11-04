6ff5ab112bc96e5b5fff5bd132c722f5576c0121
lzyprime <2383518170@qq.com>
2018-11-04 16:59:08 +0800

    server :addlog quit sentcount wait client

--- tserver.c
+++ tserver.c
@@ -1,176 +1,157 @@
-#include<time.h>
-#include<stdio.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h> 
-#include<string.h>
-#include<stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #define WAITNUM 5
 #define MAXLINE 2048
 #define MAXMEM 10
 #define NAMELEN 20
+#define SERVERADDR "192.168.207.132"
+#define SERVERPORT 16667
 
 int socket_fd;
-int connect_fd[MAXMEM];
+struct sockaddr_in servaddr;
+int connect_fd[MAXMEM], connect_count;
 int a;
-void quit();
-void recv_send(void* arg);
 
+void addlog(char *);
+void *quit();
+void *recv_send(void *arg);
+void *sendcount();
 int main()
 {
-	pthread_t thread;
-	struct sockaddr_in servaddr, client_addr;
-	socklen_t len;
-    time_t ticks;
-    char buff[MAXLINE];
-
-
-    printf("Socket...\n");
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_fd < 0)
+    // creat socket
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("Socket created failed.\n");
+        addlog("Socket created failed.");
         return -1;
     }
 
-
-    printf("Bind...\n");
+    // bind ip port
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(16666);
+    servaddr.sin_port = htons(SERVERPORT);
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if(bind(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+    if (bind(socket_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
-        printf("Bind failed.\n");
+        addlog("Bind failed.");
         return -1;
     }
 
-    printf("listening...\n");
-    
-    if(listen(socket_fd, WAITNUM) < 0)
+    // listen
+    if (listen(socket_fd, WAITNUM) < 0)
     {
-	printf("listen failed.\n");
+        addlog("listen failed.");
         return -1;
     }
 
-    pthread_create(&thread, NULL, (void*)(&quit), NULL);
-    printf("create_quit_pthread\n");
-    
-	int i;
-    for(i=0;i<MAXMEM;i++)
+    // creat listen quit() , sendcount()
+    memset(connect_fd, -1, sizeof(connect_fd));
+    pthread_create(malloc(sizeof(pthread_t)), NULL, quit, NULL);
+    pthread_create(malloc(sizeof(pthread_t)), NULL, sendcount, NULL);
+    // wait clients
+    struct sockaddr_in client_addr;
+    while (1)
     {
-        connect_fd[i]=-1;
-    }
-    memset(&client_addr, 0, sizeof(client_addr));	
-    while(1)
-    {
-        len = sizeof(client_addr);
-        for(i=0; i < MAXMEM; i++)
+        memset(&client_addr, 0, sizeof(client_addr));
+        socklen_t len = sizeof(client_addr);
+        int newclient = accept(socket_fd, (struct sockaddr *)&client_addr, &len);
+        if (newclient < 0)
+            continue;
+        else if (connect_count >= MAXMEM)
         {
-            if(connect_fd[i]==-1)
+            write(newclient, "server is full", 15);
+            close(newclient);
+        }
+        else
+        {
+            for (a = 0; a < MAXMEM; a++)
+                if (connect_fd[a] == -1)
+                    break;
+            if (a < MAXMEM)
             {
-                break;
+                pthread_create(malloc(sizeof(pthread_t)), NULL, recv_send, (void *)(&a));
+                connect_count++;
             }
         }
-
-        connect_fd[i] = accept(socket_fd, (struct sockaddr*)&client_addr, &len);
-
-		if (connect_fd[i] == -1 )
-		{
-			printf("accept failed\n");
-		}
-
-        ticks = time(NULL);
-        printf("%s Connect from: %s\n", ctime(&ticks), 
-			   inet_ntoa(client_addr.sin_addr));
-	    
-        printf("user id is %d join\n",i);
-        a = i;
-        pthread_create(malloc(sizeof(pthread_t)), NULL, (void*)(&recv_send), (void*)(&a));
-
     }
     return 0;
 }
 
-void quit()
+void *sendcount()
 {
-    char msg[10];
-    while(1)
+    while (1)
     {
-        scanf("%s",msg);
-        if(strcmp("quit",msg)==0)
+        while (!connect_count)
+            ;
+        char log[100] = {0};
+        sprintf(log, "server: online: %d", connect_count);
+        addlog(log);
+        for (int i = 0; i < MAXMEM; i++)
+            if (connect_fd[i] != -1)
+                write(connect_fd[i], log, strlen(log) + 1);
+        sleep(5);
+    }
+}
+
+void *quit()
+{
+    addlog("server start success");
+    while (1)
+    {
+        char msg[MAXLINE] = {0};
+        scanf("%s", msg);
+        char log[MAXLINE] = "server: ";
+        strcat(log, msg);
+        if (strlen(msg))
         {
-            printf("Byebye...\n");
+            addlog(log);
+            for (int i = 0; i < MAXMEM; i++)
+                if (connect_fd[i] != -1)
+                    write(connect_fd[i], msg, strlen(log));
+        }
+        if (!strcmp("quit", msg))
+        {
+            sleep(1);
             close(socket_fd);
             exit(0);
         }
     }
 }
 
-void recv_send(void *arg)
+void addlog(char text[])
 {
-    int id = *(int *)arg;
-    char* ask=">>>>>Your name please:";
-    char buff[MAXLINE];
-    char recv_buff[MAXLINE];
-    char time_buff[MAXLINE];
-    char name[NAMELEN];
-    time_t ticks;
-    
-    int retval;
-    printf("id is %d\n",id);
-    write(connect_fd[id], ask, strlen(ask));
-    int len;
-    len = read(connect_fd[id], name, NAMELEN);
-    if(len > 0)
-    {
-         name[len] = '\0';
-    }
-
-    strcpy(buff, name);
-    strcat(buff, "join in\0");
-    int i;
-    for(i=0;i<MAXMEM;i++)
-    {
-        if(connect_fd[i]!=-1)
-        {
-            write(connect_fd[i],buff,strlen(buff));
-        }
-    }
-
-    while(1)
-    {
-        if((len = read(connect_fd[id], recv_buff, MAXLINE))>0)
-        {
-            recv_buff[len]=0;
-
-             if(strcmp("bye", recv_buff) == 0)
-             {
-                 close(connect_fd[id]);
-                 connect_fd[id]=-1;
-                 pthread_exit(&retval);
-             }
-
-             ticks=time(NULL);
-             sprintf(time_buff,"%.24s\r\n",ctime(&ticks));
-             write(connect_fd,time_buff,strlen(time_buff));
-
-             strcpy(buff,name);
-             strcat(buff,"---");
-             strcat(buff,time_buff);
-             strcat(buff,recv_buff);
-
-            for(i=0;i<MAXMEM;i++)
-            {
-                 if(connect_fd[i]!=-1)
-                 {
-                      write(connect_fd[i],buff,strlen(buff));
-                 }
-            }
-        }
-
-    }
+    time_t tt;
+    FILE *log_fd = 0;
+    if ((log_fd = fopen("./socket.log", "a")) != 0)
+        tt = time(0), fprintf(log_fd, "%s %s\n", ctime(&tt), text);
+    printf("%s \n", text);
+    fclose(log_fd);
 }
 
+void *recv_send(void *arg)
+{
+    int id = *(int *)arg;
+    int client = connect_fd[id];
+    char inputext[MAXLINE];
+    while (1)
+    {
+        int len = read(client, inputext, MAXLINE);
+        if (len)
+        {
+            inputext[len] = 0;
+            char log[MAXLINE] = "client :";
+            strcat(log, inputext);
+            addlog(log);
+            for (int i = 0; i < MAXMEM; i++)
+                if (i != id && connect_fd[i] != -1)
+                    write(connect_fd[i], inputext, strlen(inputext));
+        }
+    }
+}
